749350c72ec934e8cbafde8b293d0fd3ad3b9a90
luebby <karsten@statistix.org>
2021-10-01 12:23:01 +0200

    Color temperature

--- temperature.R
+++ temperature.R
@@ -25,12 +25,15 @@ soni_temp <- sonify(y = Temperature$avg.temperature,
 writeWave(soni_temp, "temperature.wav")
 
 # Plotting
-p_temp <- gf_line(avg.temperature ~ year , data = Temperature, size = 2) %>%
+p_temp <- gf_line(avg.temperature ~ year , data = Temperature, size = 2,
+                  color = ~ avg.temperature) %>%
+  gf_refine(scale_color_distiller(palette = "RdBu")) %>%
   gf_theme(panel.background=element_blank()) +
+  theme(legend.position = "none") +
   ggtitle("Warming in Germany") +
   labs(caption = "Source: https://opendata.dwd.de/", y="Average temperature °C (5 years)")
 
-# Anomation
+# Animation
 anim_temp <- p_temp + transition_reveal(along=year)
 anim_save("temp.gif", animate(anim_temp, duration = duration, fps = 10))
 
@@ -39,3 +42,5 @@ anim_save("temp.gif", animate(anim_temp, duration = duration, fps = 10))
 system("ffmpeg -f gif -i temp.gif tempdummy.mp4")
 # Combine video (gif) and sound (wav)
 system("ffmpeg -i tempdummy.mp4 -i temperature.wav -c:v copy -c:a aac warming.mp4")
+
+# ffmpeg -i warming.mp4 -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p -strict experimental -r 30 -t 2:20 -acodec aac -vb 1024k -minrate 1024k -maxrate 1024k -bufsize 1024k -ar 44100 -ac 2 out.mp4
